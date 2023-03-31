c241aeb6cc221133eea31dd84a9dde168d382c00
KesterDaniel <kesterdan17@gmail.com>
2023-03-31 12:54:52 +0100

    adding books dictionary to all books list

--- main.py
+++ main.py
@@ -7,12 +7,20 @@ all_books = []
 
 @app.route('/')
 def home():
-    pass
+    return render_template("index.html")
 
 
-@app.route("/add")
+@app.route("/add", methods=["GET", "POST"])
 def add():
-    pass
+    if request.method == "POST":
+        book_dict = {
+            "title": request.form["name"],
+            "author": request.form["author"],
+            "rating": request.form["rating"]
+        }
+        all_books.append(book_dict)
+
+    return render_template("add.html")
 
 
 if __name__ == "__main__":
